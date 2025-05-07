5f24cbed5aec3de506d8bb688f95685dea6608ce
zwoong <zwoong6867@gmail.com>
2025-05-07 14:14:32 +0900

    ✨ 롬복과 최신 트렌드

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,29 +3,14 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
-
-
-//  @Autowired
-//  public void setMemberRepository(MemberRepository memberRepository) {
-//    this.memberRepository = memberRepository;
-//  }
-//
-//  @Autowired
-//  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//    this.discountPolicy = discountPolicy;
-//  }
-
-  public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-    this.memberRepository = memberRepository;
-    this.discountPolicy = discountPolicy;
-  }
 
   @Override
   public Order createOrder(Long memberId, String itemName, int itemPrice) {
@@ -35,8 +20,4 @@ public class OrderServiceImpl implements OrderService {
     return new Order(memberId, itemName, itemPrice, discountPrice);
   }
 
-  // 테스트 용도
-  public MemberRepository getMemberRepository() {
-    return memberRepository;
-  }
 }
