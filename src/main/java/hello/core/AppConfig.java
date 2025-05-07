7730328b0e0ca5dd72fe8fed4421d1ea8cc44a13
zwoong <zwoong6867@gmail.com>
2025-05-07 13:19:18 +0900

    ✨ 다양한 의존관계 주입 방법

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -30,7 +30,8 @@ public class AppConfig {
   @Bean
   public OrderService orderService() {
     System.out.println("AppConfig.orderService");
-    return new OrderServiceImpl(memberRepository(), discountPolicy());
+    return null;
+//    return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
   @Bean
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,11 +8,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class OrderServiceImpl implements OrderService {
-
   private final MemberRepository memberRepository;
   private final DiscountPolicy discountPolicy;
 
-  @Autowired
+
+//  @Autowired
+//  public void setMemberRepository(MemberRepository memberRepository) {
+//    this.memberRepository = memberRepository;
+//  }
+//
+//  @Autowired
+//  public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//    this.discountPolicy = discountPolicy;
+//  }
+
   public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
     this.memberRepository = memberRepository;
     this.discountPolicy = discountPolicy;
