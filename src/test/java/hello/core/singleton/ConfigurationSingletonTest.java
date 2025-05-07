12834619e945666cbf2cc8d5194cc29283a61295
zwoong <zwoong6867@gmail.com>
2025-05-07 14:20:12 +0900

    ✨ 조회 빈이 2개 이상 - 문제

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -21,14 +21,11 @@ public class ConfigurationSingletonTest {
     MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
 
     MemberRepository memberRepository1 = memberService.getMemberRepository();
-    MemberRepository memberRepository2 = orderService.getMemberRepository();
 
     System.out.println("memberService -> memberRepository = " + memberRepository1);
-    System.out.println("orderService -> memberRepository " + memberRepository2);
     System.out.println("memberRepository = " + memberRepository);
 
     assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
-    assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
   }
   
   @Test
