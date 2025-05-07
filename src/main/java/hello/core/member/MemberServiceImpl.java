3d5e2a6439b10b0c7a7d3f6e1c3bb82a66e9690a
zwoong <zwoong6867@gmail.com>
2025-05-07 20:06:09 +0900

    ✨  request 스코프 예제 만들기

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,6 +1,7 @@
 package hello.core.member;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -30,19 +31,17 @@ public class SingletonWithPrototypeTest1 {
     Assertions.assertThat(clientBean1.logic()).isEqualTo(1);
 
     ClientBean clientBean2 = ac.getBean(ClientBean.class);
-    Assertions.assertThat(clientBean2.logic()).isEqualTo(2);
+    Assertions.assertThat(clientBean2.logic()).isEqualTo(1);
   }
 
   @Scope("singleton")
   static class ClientBean {
-    private final PrototypeBean prototypeBean;
 
     @Autowired
-    public ClientBean(PrototypeBean prototypeBean) {
-      this.prototypeBean = prototypeBean;
-    }
+    private ObjectProvider<PrototypeBean> prototypeBeanProvider;
 
     public int logic() {
+      PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
       prototypeBean.addCount();
       int count = prototypeBean.getCount();
       return count;
